55d3b99b47659b233718bde36a4a478f8dde87a0
Galaadseraph <galaad.seraphin@gmail.com>
2017-01-27 16:28:04 +0100

    Un pion basique sauvage apparait

---
+++ src/fr/imie/echiquier/Pion.java
@@ -0,0 +1,46 @@
+package fr.imie.echiquier;
+
+public class Pion extends Piece
+{
+	//Region Deplacement
+	public void Deplacement(Coordonnees cible) throws EchiquierException
+	{
+		//Dans la même colonne
+		if (this.position.getAbscisse() == cible.getAbscisse())
+		{
+			int difference = this.position.getOrdonne() - cible.getOrdonne();
+			if (this.couleur == "Blanc")
+			{
+				if (difference==-1)
+				{
+					if (positionPossible(cible))
+					{
+						this.position = cible;
+					}
+				}
+				else throw new EchiquierException("Deplacement invalide");
+			}
+			else //Pour les noirs
+			{
+				if (difference==1)
+				{
+					if (positionPossible(cible))
+					{
+						this.position = cible;
+					}
+				}
+				else throw new EchiquierException("Deplacement invalide");
+			}
+		}
+		else throw new EchiquierException("Deplacement invalide");
+		// pas la même colonne
+	}
+	//EndRegion
+	
+	public Pion(Coordonnees position, String couleur)
+	{
+		this.position = position;
+		this.couleur = couleur;
+		this.type = CodePiece.Pion;
+	}
+}
