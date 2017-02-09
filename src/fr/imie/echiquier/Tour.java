5fd2ecc82de5b9a873d799d1c735605ce3fddf04
Galaadseraph <galaad.seraphin@gmail.com>
2017-02-09 09:46:53 +0100

    Tour construite et prète à tirer

---
+++ src/fr/imie/echiquier/Tour.java
@@ -0,0 +1,42 @@
+package fr.imie.echiquier;
+
+public class Tour extends Piece
+{
+	//Region Deplacement
+		public void Deplacement(Coordonnees cible) throws EchiquierException
+		{
+			//Dans la même colonne
+			if (this.position.getAbscisse() == cible.getAbscisse())
+			{
+				int difference = this.position.getOrdonne() - cible.getOrdonne();
+				if (difference==0)
+				{
+					if (positionPossible(cible))
+					{
+						this.position = cible;
+					}
+				}
+				else throw new EchiquierException("Deplacement invalide");
+			}
+			else // pas la même colonne
+			{
+				int difference = this.position.getAbscisse() - cible.getAbscisse();
+				if (difference==0)
+				{
+					if (positionPossible(cible))
+					{
+						this.position = cible;
+					}
+				}
+				else throw new EchiquierException("Deplacement invalide");
+			}
+		}
+		//EndRegion
+		
+		public Tour(Coordonnees position, String couleur)
+		{
+			this.position = position;
+			this.couleur = couleur;
+			this.type = CodePiece.Tour;
+		}
+}
